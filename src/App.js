b67a54be67cd48ef158f99599cae4eff64d2928f
KazStan19 <93115335+KazStan19@users.noreply.github.com>
2022-01-12 11:40:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import Test from './components/test'
+
 
 export default function App() {
     return (
         <div>  
-            <Test />
+
         </div>
     )
 }
