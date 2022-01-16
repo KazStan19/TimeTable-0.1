83fb1d2c3307e4d762a48e51b64cf82f12f56cd2
codingfullstack <justinas.sadauskiu@gmail.com>
2022-01-16 20:36:25 +0200

    darbu forma

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import CreateForm from './component/CreateForm';
+import { Container } from 'react-bootstrap';
+
 
 
 export default function App() {
     return (
-        <div>  
-
-        </div>
+        <Container>
+        <CreateForm />
+        </Container>
     )
 }
