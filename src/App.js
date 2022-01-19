3996b9b0a28b9e3bccc89c9d0b57c780c3d190ea
KazStan19 <93115335+KazStan19@users.noreply.github.com>
2022-01-19 08:40:31 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
 import React from 'react'
-import Register from './components/Register/Register'
-import 'bootstrap/dist/css/bootstrap.min.css';
+
 export default function App() {
     return (
         <div>  
-            <Register />
+           
         </div>
     )
 }
